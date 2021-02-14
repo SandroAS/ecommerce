007a068a4b9783c264edcdbd969434f4502b78ce
Sandro Souza <sandroantoniosouza98@gmail.com>
2021-02-14 12:17:59 -0300

    Aula 106 Login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
+//require_once("functions.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
-use \Hcode\PageAdmin;
+use \Hcode\PageAmdin;
+use Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -12,7 +14,7 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Page();
+	$page = new Hcode\Page();
 
 	$page->setTpl("index");
 
@@ -20,9 +22,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
-	$page = new Page();
+	User::verifyLogin();
+
+	$page = new Hcode\PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
